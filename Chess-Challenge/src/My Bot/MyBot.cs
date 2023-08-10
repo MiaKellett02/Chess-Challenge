7702c0c41739137ccbcc9de8603b1c17403299df
Mia Kellett <jackkellett02@gmail.com>
2023-08-10 19:50:06 +0100

    Added recursive Evaluation.
    Added recursive Evaluation:
     - Evaluation on an initial argument depth of 1 works the same as before.
     - On an initial depth of more than 1 it gets all the next possible moves, evaluates each of them at -1 the original depth until it has check the original depth down every branch of every possible move the enemy could make. It inverts the score as what's good for the enemy is bad for the player. Then it adds the least bad score to the players score and exits.
     - The current version has a recursive depth of 5.

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -3,10 +3,15 @@ using System;
 using System.Numerics;
 using System.Collections.Generic;
 using ChessChallenge.Application;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 public class MyBot : IChessBot {
+	//Consts.
+	private const int EVALUATION_RECURSIVE_DEPTH = 5;
+
+	//Variables.
 	private Board m_board;
-	private bool isWhite;
+	private bool isMyBotWhite;
 
 	//Debug variables.
 	private int highestValueLastTime;
@@ -19,14 +24,14 @@ public class MyBot : IChessBot {
 		Move[] moves = m_board.GetLegalMoves();
 
 		//Get the colour of MyBot.
-		isWhite = m_board.IsWhiteToMove;
+		isMyBotWhite = m_board.IsWhiteToMove;
 
 		//Evalulate each move and choose best one.
 		int highestValue = int.MinValue;
 		Move bestMove = moves[0];
 		foreach (Move move in moves) {
 			//Evaluate the move.
-			int value = Evaluate(move, 1);
+			int value = Evaluate(move, EVALUATION_RECURSIVE_DEPTH);
 			if (value > highestValue) {
 				bestMove = move;
 				highestValue = value;
@@ -44,16 +49,24 @@ public class MyBot : IChessBot {
 	}
 
 	public int Evaluate(Move a_move, int a_deepness) {
+		int currentDepth = a_deepness - 1;
 		//Initalise return value.
 		int boardValueAfterMove = 0;
+
+		//Get the current turn's colour.
+		bool currentTurnIsWhite = m_board.IsWhiteToMove;
 
 		//Make move then get the score of the state of the board afterwards.
 		m_board.MakeMove(a_move);
 
+		//Get the next turns colour.
+		bool nextTurnIsWhite = m_board.IsWhiteToMove;
+
+		int CHECKMATE_VALUE = 1000000;
 		if (m_board.IsInCheckmate()) {
 			//Always do checkmate.
 			m_board.UndoMove(a_move);
-			return int.MaxValue;
+			return CHECKMATE_VALUE;
 		}
 
 		int CHECK_VALUE = 100;
@@ -68,7 +81,7 @@ public class MyBot : IChessBot {
 
 		int BLACK_MULTIPLIER;
 		int WHITE_MULTIPLIER;
-		if (isWhite) {
+		if (currentTurnIsWhite) {
 			//Count white pieces vs black pieces.
 			//White pieces give positive value and black pieces give negative value.
 			BLACK_MULTIPLIER = -1;
@@ -88,6 +101,24 @@ public class MyBot : IChessBot {
 			} else {
 				boardValueAfterMove += (BLACK_MULTIPLIER * pieces.Count);
 			}
+		}
+
+		if (currentDepth > 0) {
+			//Get list of next posisble moves.
+			Move[] nextMoves = m_board.GetLegalMoves();
+
+			//Evaluate each of those moves with 1 less depth than the previous call of evaluate.
+			//When it reaches 0 then the recursive loop will exit with the best approximate move.
+			int highestScore = int.MinValue;
+			foreach (Move move in nextMoves) {
+				int moveScore = -Evaluate(move, currentDepth); //Inverts the evaluation score as what's best for the next player won't be best for the current player.
+				if (moveScore > highestScore) {
+					highestScore = moveScore;
+				}
+			}
+
+			//Set the main score to the total as we want to ignore the current state of the board and only think of the best state further on in the board.
+			boardValueAfterMove = highestScore;
 		}
 
 		//Return board to original state.
