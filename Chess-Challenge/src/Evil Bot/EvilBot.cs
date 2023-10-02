13eb78a6a2fb868915d3adc431e0f9f4aa516c3a
Mia Kellett <jackkellett02@gmail.com>
2023-10-02 14:03:54 +0100

    Revert "AlfieBotv4.2.0"
    This reverts commit b44394d2bfe35c11cb0aad23b9a055c6d79b88f2.

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -4,7 +4,7 @@ using System.Numerics;
 
 namespace ChessChallenge.Example {
 	//===============================
-	//AlfieBotv4.2.0
+	//Alfie Bot 4.2.0
 	//===============================
 
 	public class EvilBot : IChessBot {
