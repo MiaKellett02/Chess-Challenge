6c9d0b9b27188628f8de64a55d59cd74d949e618
Mia Kellett <jackkellett02@gmail.com>
2023-10-02 14:07:09 +0100

    evil bot replaced with my bot v3.0

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -6,226 +6,251 @@ using System.Numerics;
 
 namespace ChessChallenge.Example {
 	//================================
-	//JamesBotv22.28-01-10-2023
+	//MiaKBotv3.0
 	//================================
 
 	public class EvilBot : IChessBot {
-		public static class Def // AKA - the magic-number zone
-		{
-			public static int[] pieceValues = { 0, 100, 900, 900, 2500, 8100, 80000 }; // these values are squared to exponentially prioritise higher value pieces
-			public static int moveBudget = 20000;
-			public static int moveLog = 0;
-			public static int maxDepth = 6;
-		}
-		public struct MOVE // a Move with a score
-		{
-			float score;
-			public Move move;
-			public int interest;
-			public int Budget;
-			public MOVE(Move i, float j, int k) {
-				move = i;
-				score = j;
-				interest = k;
-				Budget = 0;
+	//Consts.
+	private const int EVALUATION_RECURSIVE_DEPTH = 3;//This is how many moves ahead the bot will think about.
+													 //The consts after this line are values of a move based on the state of the board after that move 
+	private const int NO_ENEMY_CAPTURE_VALUE = -1000000; //When a move doesn't capture anything it is given this weight.
+	private const int ENEMY_CAPTURED_VALUE = 10000;
+	private const int CHECKMATE_VALUE = 1000000000;
+	private const int CHECK_VALUE = 100000;
+	private const int DRAW_VALUE = -100000;
+	//The consts after this line are values of a move based on if the move is a special move type.
+	private const int CASTLES_VALUE = 1000;
+	private const int ENPASSANT_VALUE = 1000;
+	private const int CAPTURE_VALUE = 500;
+	//The consts after this line are the weights added to each move when it doesn't
+	//lead to a capture depending on the piece being moved.
+	private const int KING_MOVE_SCORE_WEIGHT = -1000;
+	private const int QUEEN_MOVE_SCORE_WEIGHT = 100;
+	private const int ROOK_MOVE_SCORE_WEIGHT = 200;
+	private const int BISHOP_MOVE_SCORE_WEIGHT = 200;
+	private const int KNIGHT_MOVE_SCORE_WEIGHT = 100;
+	private const int PAWN_MOVE_SCORE_WEIGHT = 1000;
 
+	//Variables.
+	private Board m_board;
+	private int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };// Piece values: null, pawn, knight, bishop, rook, queen, king
+	private int BLACK_MULTIPLIER;
+	private int WHITE_MULTIPLIER;
+	private bool myBotIsWhite;
+
+	//Debug variables.
+	private int highestValueLastTime;
+
+	public Move Think(Board board, Timer timer) {
+		//Cache the state of the board.
+		m_board = board;
+		myBotIsWhite = m_board.IsWhiteToMove;
+
+		//Get all the legal moves.
+		Move[] moves = m_board.GetLegalMoves();
+
+		//Evalulate each move and choose best one.
+		Random rng = new Random((int)System.DateTime.UtcNow.Ticks);
+		Move bestMove = moves[rng.Next(moves.Length)];
+		int highestValue = Evaluate(bestMove, EVALUATION_RECURSIVE_DEPTH);
+		foreach (Move move in moves) {
+			//Evaluate the move.
+			int value = Evaluate(move, EVALUATION_RECURSIVE_DEPTH);
+			if (value > highestValue) {
+				bestMove = move;
+				highestValue = value;
 			}
-
-			public void setScore(float i) => score = i;
-			public float getScore() {
-				return score;
-			}
-			public void setBudget(int totalInterest, int moveBudget) {
-				float ratio = 0;
-				if (totalInterest > 0)
-					ratio = interest / (float)totalInterest;
-				Budget = (int)(moveBudget * ratio);
-				if (Budget < 3)
-					Budget = 0;
-
-			}
-
-			public int getNumOfMoves(Board i) {
-				return i.GetLegalMoves().Length;
-			}
-		}
-
-
-
-		public class MinMaxFish {
-			public int totalInterest = 0;
-			MOVE[] getLegalMoves(Board board, float startingScore, int moveBudget) {
-				var m = board.GetLegalMoves();
-				var rng = new Random();
-				m = m.OrderBy(e => rng.NextDouble()).ToArray();
-
-
-
-				List<MOVE> moves = new List<MOVE>();
-				foreach (Move i in m) {
-
-
-					int localInterest = 1;
-					float localScore = startingScore;
-
-					if (i.MovePieceType != PieceType.Pawn && i.MovePieceType != PieceType.King) // encourage using pieces besides pawns/kings
-					{
-						localInterest++;
-						localScore += Def.pieceValues[(int)i.MovePieceType] * 0.1f;
-					}
-
-					if (i.TargetSquare.File != 0 && i.TargetSquare.File != 7) // discourage moving pieces to edge tiles
-					{
-						localScore += 5;
-					}
-
-					if (i.TargetSquare.Rank >= 2 && i.TargetSquare.Rank <= 6) // Encourage developing pieces
-					{
-						localScore += 5;
-					}
-
-					if (board.IsInCheck()) // encourage moving the king while in check
-					{
-						localScore -= 1000;
-						localInterest++;
-					}
-
-					if (i.IsCastles) // encourage castling
-					{
-						localScore += 5000;
-						localInterest++;
-					}
-					if (i.IsPromotion) // encourage promotion!
-					{
-						localScore += Def.pieceValues[(int)i.PromotionPieceType];
-						localInterest++;
-					}
-
-					if (i.IsCapture) // encourage capture
-					{
-						localScore += Def.pieceValues[(int)i.CapturePieceType] + 10000;
-						localInterest += 2;
-					} else {
-						localScore -= 5000;
-					}
-
-					if (board.SquareIsAttackedByOpponent(i.TargetSquare) || board.SquareIsAttackedByOpponent(i.StartSquare)) {
-						localInterest++;
-						//localScore -= Def.pieceValues[(int)i.MovePieceType] * 0.5f;
-					}
-
-					totalInterest += localInterest;
-					moves.Add(new MOVE(i, localScore, localInterest));
-				}
-
-				List<MOVE> budgetMoves = new List<MOVE>();
-
-				foreach (MOVE i in moves) {
-					i.setBudget(totalInterest, moveBudget);
-					budgetMoves.Add(i);
-				}
-				return budgetMoves.ToArray();
-			}
-			public MOVE Think(Board board, int moveBudget, int depth) {
-				Random rng = new Random();
-
-				PieceList[] pieces = board.GetAllPieceLists();
-
-				float whiteScore = 0;
-
-				for (int i = 0; i <= 5; i++) {
-					foreach (Piece piece in pieces[i]) {
-						whiteScore += Def.pieceValues[i + 1];
-						if (board.SquareIsAttackedByOpponent(piece.Square) && board.IsWhiteToMove) {
-							whiteScore -= Def.pieceValues[i + 1] / 10.0f;
-						}
-					}
-
-					foreach (Piece piece in pieces[i + 6]) {
-						whiteScore -= Def.pieceValues[i + 1];
-						if (board.SquareIsAttackedByOpponent(piece.Square) && !board.IsWhiteToMove) {
-							whiteScore += Def.pieceValues[i + 1] / 10.0f;
-						}
-					}
-				}
-				float score = 0;
-				if (board.IsWhiteToMove)
-					score = whiteScore;
-				else
-					score = whiteScore * -1;
-
-				MOVE[] moves = getLegalMoves(board, score, moveBudget);
-				MOVE bestMove = moves[0];
-
-				foreach (MOVE move in moves) {
-
-					board.MakeMove(move.move);
-
-					if (board.IsInCheckmate()) {
-						move.setScore(int.MaxValue);
-						depth = 0;
-					}
-					if (board.IsInStalemate() || board.IsInsufficientMaterial() || board.IsFiftyMoveDraw() || board.IsRepeatedPosition()) {
-						move.setScore(int.MinValue);
-						depth = 0;
-
-					}
-
-					if (move.Budget >= 1 && depth > 0) {
-
-						MinMaxFish i = new MinMaxFish();
-						MOVE a = i.Think(board, move.Budget, depth - 1);
-						move.setScore(a.getScore() * -1);
-					}
-					board.UndoMove(move.move);
-
-					if (move.getScore() > bestMove.getScore()) {
-						bestMove = move;
-					}
-
-					Def.moveLog += 1;
-
-					//            if (depth ==  Def.maxDepth)
-					//            {
-					//                ConsoleHelper.Log(
-					//  "Move: " + Def.moveLog + '|' + bestMove.move.MovePieceType.ToString() + " to " + bestMove.move.TargetSquare.ToString() +
-					//  "\nInterest: " + bestMove.interest +
-					//  ", Score:" + bestMove.getScore() +
-					//  ", Budget:" + bestMove.Budget +
-					//    ", Depth:" + (Def.maxDepth - depth)
-					//, false, (ConsoleColor)depth);
-					//            }
-
-
-				}
-
-
-
-
-				return bestMove;
-
-			}
-
-
 		}
 
-
-		public Move Think(Board board, Timer timer) {
-
-
-
-			MinMaxFish fisher = new MinMaxFish();
-
-			Def.moveLog = 0;
-			// float x = (timer.MillisecondsRemaining / 60000.0f);
-			//  x = (-4 * (float)Math.Pow(x, 2)) + (4 * x);
-			//   x = (x * 0.75f) + 0.25f;
-			int timeBudget = Def.moveBudget;
-
-			MOVE bestMove = fisher.Think(board, timeBudget, Def.maxDepth);
-			return bestMove.move;
-
+		//DEBUG
+		if (highestValueLastTime != highestValue) {
+			highestValueLastTime = highestValue;
+			ChessChallenge.Application.ConsoleHelper.Log("HighestValue: " + highestValue.ToString());
 		}
 
+		//Return the move to make.
+		return bestMove;
 	}
+
+	public int Evaluate(Move a_move, int a_deepness) {
+		PieceType movePieceType = a_move.MovePieceType;
+		int capturedPieceValue = pieceValues[(int)m_board.GetPiece(a_move.TargetSquare).PieceType];
+		int currentDepth = a_deepness - 1;
+		//Initalise return value.
+		int moveEvaluationScore = 0;
+
+		//Get the current turn's colour.
+		bool currentTurnIsWhite = m_board.IsWhiteToMove;
+		bool currentTurnIsMyBot = currentTurnIsWhite == myBotIsWhite;
+		if (currentTurnIsWhite) {
+			//Count white pieces vs black pieces.
+			//White pieces give positive value and black pieces give negative value.
+			BLACK_MULTIPLIER = -1;
+			WHITE_MULTIPLIER = 1;
+		} else {
+			//Count white pieces vs black pieces.
+			//White pieces give negative value and black pieces give positive value.
+			BLACK_MULTIPLIER = 1;
+			WHITE_MULTIPLIER = -1;
+		}
+
+		//Get the original board value.
+		int boardValueBeforeMove = GetValueOfBoard(m_board);
+
+		//Make move then get the score of the state of the board afterwards.
+		m_board.MakeMove(a_move);
+
+		//Check the board for different main game states.
+		if (m_board.IsInCheckmate()) {
+			//Always do checkmate.
+			m_board.UndoMove(a_move);
+			return CHECKMATE_VALUE;
+		}
+
+		if (m_board.IsInCheck()) {
+			moveEvaluationScore += CHECK_VALUE;
+		}
+
+		if (m_board.IsDraw()) { //Stalemate, draw, repetition, insuffcient material etc...
+			m_board.UndoMove(a_move);
+			moveEvaluationScore += DRAW_VALUE;
+			return moveEvaluationScore;
+		}
+
+		if (a_move.IsCastles) {
+			moveEvaluationScore += CASTLES_VALUE;
+		}
+
+		if (a_move.IsEnPassant) {
+			moveEvaluationScore += ENPASSANT_VALUE;
+		}
+
+		if (a_move.IsCapture) {
+			moveEvaluationScore += CAPTURE_VALUE;
+		}
+
+		//Get the value of the whole board if the move is made.
+		int valueOfBoardIfMoveIsMade = GetValueOfBoard(m_board);
+		int netMoveScore = (valueOfBoardIfMoveIsMade - boardValueBeforeMove);
+		if (netMoveScore <= 0) {
+			//If move does not capture a piece.
+			moveEvaluationScore += NO_ENEMY_CAPTURE_VALUE; //Discourage bot from not capturing.
+
+			//Then give it more incentive to move certain pieces in that case.
+			switch (movePieceType) {
+				case PieceType.King: {
+					moveEvaluationScore += KING_MOVE_SCORE_WEIGHT;
+					break;
+				}
+				case PieceType.Queen: {
+					moveEvaluationScore += QUEEN_MOVE_SCORE_WEIGHT;
+					break;
+				}
+				case PieceType.Rook: {
+					moveEvaluationScore += ROOK_MOVE_SCORE_WEIGHT;
+					break;
+				}
+				case PieceType.Bishop: {
+					moveEvaluationScore += BISHOP_MOVE_SCORE_WEIGHT;
+					break;
+				}
+				case PieceType.Knight: {
+					moveEvaluationScore += KNIGHT_MOVE_SCORE_WEIGHT;//Initial move weight.
+
+					//Move weight to be added depedning on if the knight is close to the center.
+					int KNIGHT_MOVE_TO_CENTER_WEIGHT = 1000;
+					bool isCloseToCenter = SquareIsCloseToCenter(a_move.TargetSquare);
+					if (isCloseToCenter) {
+						moveEvaluationScore += KNIGHT_MOVE_TO_CENTER_WEIGHT;
+					} else {
+						moveEvaluationScore += (-KNIGHT_MOVE_TO_CENTER_WEIGHT);
+					}
+					break;
+				}
+				case PieceType.Pawn: {
+					moveEvaluationScore += PAWN_MOVE_SCORE_WEIGHT;
+
+					//Move weight to be added depending on if the pawn is close to an end rank.
+					int PAWN_MOVE_TO_END_RANK_WEIGHT = 500;
+					bool pawnIsNotOnEndRank = IsSquareAnEndRank(a_move.StartSquare);
+					if (pawnIsNotOnEndRank) {
+						moveEvaluationScore += PAWN_MOVE_TO_END_RANK_WEIGHT;
+					} else {
+						moveEvaluationScore += (-PAWN_MOVE_TO_END_RANK_WEIGHT);
+					}
+					break;
+				}
+				default: {
+					break;
+				}
+
+			}
+		} else {
+
+			if (currentTurnIsMyBot) {
+				moveEvaluationScore += ENEMY_CAPTURED_VALUE * capturedPieceValue;
+			} else {
+				//If the current turn isn't my bot I want the enemy captured value to be higher so mybot is discourage from making the move that allowed that to happen.
+				moveEvaluationScore += (ENEMY_CAPTURED_VALUE * capturedPieceValue * 10);
+			}
+
+		}
+
+		if (currentDepth > 0) {
+			//Get list of next posisble moves.
+			Move[] nextMoves = m_board.GetLegalMoves();
+
+			//Evaluate each of those moves with 1 less depth than the previous call of evaluate.
+			//When it reaches 0 then the recursive loop will exit with the best approximate move.
+			int worstScoreForPreviousPlayer = int.MaxValue;
+			foreach (Move move in nextMoves) {
+				int moveScore = -Evaluate(move, currentDepth); //Inverts the evaluation score as what's best for the next player won't be best for the current player.
+				if (moveScore < worstScoreForPreviousPlayer) {
+					worstScoreForPreviousPlayer = moveScore;
+				}
+			}
+
+			//add the score to the moves score.
+			moveEvaluationScore += worstScoreForPreviousPlayer;
+		}
+
+
+		//Return board to original state.
+		m_board.UndoMove(a_move);
+
+		//Return the value.
+		return moveEvaluationScore;
+	}
+
+	private int GetValueOfBoard(Board a_board) {
+		PieceList[] allPieces = m_board.GetAllPieceLists();
+		int boardValue = 0;
+		foreach (PieceList pieces in allPieces) {
+			bool isWhitePieceList = pieces.IsWhitePieceList;
+			int piecesCountTimesPieceValue = pieces.Count * pieceValues[(int)pieces.TypeOfPieceInList];
+			if (isWhitePieceList) {
+				boardValue += (WHITE_MULTIPLIER * piecesCountTimesPieceValue);
+			} else {
+				boardValue += (BLACK_MULTIPLIER * piecesCountTimesPieceValue);
+			}
+		}
+		return boardValue;
+	}
+
+	private bool SquareIsCloseToCenter(Square a_square) {
+		if (a_square.File > 1 && a_square.File < 6 && a_square.Rank > 1 && a_square.Rank < 6) {
+			return true; //Square is close to the center of the board.
+		}
+
+		return false;//Square is not close to the center of the board.
+	}
+
+	private bool IsSquareAnEndRank(Square a_square) {
+		if (a_square.Rank <= 0 || a_square.Rank >= 7) {
+			return true;//Square is an end rank.
+		}
+
+		return false;//Square is not an end rank.
+	}
+}
 }
