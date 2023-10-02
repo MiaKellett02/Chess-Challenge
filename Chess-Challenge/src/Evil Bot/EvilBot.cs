36ec84ef74682dd3365f869e27c3728ceda9a7a4
Mia Kellett <jackkellett02@gmail.com>
2023-10-02 14:04:06 +0100

    Revert "AlfieBotv03.33-01-10-2023"
    This reverts commit b704d29e4dba0b7881f2380718f911c91c6ff79f.

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -1,279 +1,266 @@
 ﻿using ChessChallenge.API;
 using System;
-using System.Numerics;
+using System.Linq;
+
+using System.Collections.Generic;
+
+
+// remove when not debugging
+using ChessChallenge.Application;
 
 namespace ChessChallenge.Example {
 
-	//===============================
-	//AlfieBotv03.33-01-10-2023
-	//===============================
-
+	/// <summary>
+	// JamesBot10.30-01-10-2023
+	/// </summary>
 	public class EvilBot : IChessBot {
-		//Value of pieces for taking and protecting.
-		//Value of pieces for taking and protecting - None, Pawn, Knight, Bishop, Rook, King, Queen.
-		static public int[] pieceValues = { 0, 120, 175, 275, 275, 400, 1500 };
+    public static class Def // AKA - the magic-number zone
+    {
+        public static int[] pieceValues = { 0, 10, 90, 90, 250, 810, 810 }; // these values are squared to exponentially prioritise higher value pieces
+        public static int moveBudget = 40000;
+        public static int moveAvg = 1;
+        public static int maxDepth = 2;
+    }
+    public struct MOVE // a Move with a score
+    {
+        int score;
+        public Move move;
+        public int interest;
+        public int Budget;
+        public MOVE(Move i, int j, int k)
+        {
+            move = i;
+            score = j;
+            interest = k;
+            Budget = 0;
 
-		//Cost of moving a piece, to discourage moving high value pieces - None, Pawn, Knight, Bishop, Rook, King, Queen.
-		static public int[] pieceMoveCost = { 0, 25, 40, 50, 50, 100, 175 };
+        }
 
+        public void setScore(int i) => score = i;
+        public int getScore() { return score; }
+        public void setBudget(int totalInterest, int moveBudget)
+        {
+            float ratio = 0;
+            if (totalInterest > 0) ratio = interest / (float)totalInterest;
+            Budget = (int)(moveBudget * ratio);
 
-		//Interest level of a move adds additional levels of recursion to a move
-		public enum Interest {
-			NONE = 0,
-			LOW = 1,
-			MEDIUM = 2,
-			HIGH = 3,
-		}
-
-		//Scores awarded for various states of the game.
-		const int checkValue = 50;
-		const int checkMateValue = 1000000;
-		const int potentialCheckmateValue = 400;
-		const int drawValue = -10000000;
-
-		//Bonuses given to special moves.
-		const int promotionBonus = 100;
-		const int enPassantBonus = 100;
-		const int castleBonus = 75;
-
-		//Rewards for tempting pieces into certain areas
-		const int closerToCentreBonus = 25;
-
-		//Base level of recursion to use when evaluating a move.
-		const int baseMoveDepth = 3;
-		const int maxMoveDepth = 6;
-		const int inspectFurtherThreshold = 300;
-
-		//Whether its the players turn or not.
-		static bool isMyTurn;
-		//Multiplier that sets preference to protecting pieces on the players turn.
-		const float myPieceMultiplier = 1.4f;
-		//Multiplier that is added to weight the enemies move higher than the players move
-		const float enemyTurnMultiplier = 1f;
-
-		static Random rng;
-
-		//An evaluated move with a score, a level of interest and an assigned move
-		public struct EvaluatedMove {
-			//Score - How valuable this move is to the current player.
-			public int score;
-			//Intrest - How intresting a move is which can be used as a bonus to provide deeper levels of recursion.
-			public Interest interest;
-			//Move - The move that is made using the chess challenge API.
-			public Move move;
-
-			public EvaluatedMove(Move _move) {
-				score = 0;
-				interest = Interest.NONE;
-				move = _move;
-			}
-		}
-
-		public Move Think(Board board, Timer timer) {
-			//Upon begining to think it should be the players turn.
-			isMyTurn = true;
-
-			//Get All Moves
-			Span<Move> moveSpan = stackalloc Move[500];
-			board.GetLegalMovesNonAlloc(ref moveSpan);
-
-			Span<EvaluatedMove> evaluatedMovesSpan = stackalloc EvaluatedMove[moveSpan.Length];
-			var evaluatedMoves = EvaluateMoves(moveSpan, evaluatedMovesSpan, board, timer);
-
-			//Get the highest scoring move from all the evaluated moves, this will (hopefully) be the optimal move
-			EvaluatedMove bestMove = GetBestMove(evaluatedMoves);
-
-			Console.WriteLine($"Best move score: {bestMove.score}");
-			return bestMove.move;
-		}
-
-		public Span<EvaluatedMove> EvaluateMoves(Span<Move> moveSpan, Span<EvaluatedMove> evaluatedMovesSpan, Board board, Timer timer, int currentDepth = 1, int recursiveDepth = baseMoveDepth) {
-			//Consider all legal moves, with current and max depth set, by default this is base depth
-			for (int i = 0; i < evaluatedMovesSpan.Length; i++) {
-				evaluatedMovesSpan[i] = EvaluateMove(moveSpan[i], board, timer, currentDepth, recursiveDepth);
-			}
-
-			return evaluatedMovesSpan;
-		}
-
-		public static EvaluatedMove GetBestMove(Span<EvaluatedMove> evaluatedMovesSpan) {
-			//Chooses a random move, if no move is deemed better, this is the move that is made.
-			Random rng = new();
-			EvaluatedMove bestMove = evaluatedMovesSpan[rng.Next(evaluatedMovesSpan.Length)];
-
-			//Gets the best move
-			for (int i = 0; i < evaluatedMovesSpan.Length; i++) {
-				if (isMyTurn) {
-					if (evaluatedMovesSpan[i].score > bestMove.score)
-						bestMove = evaluatedMovesSpan[i];
-				} else {
-					if (evaluatedMovesSpan[i].score < bestMove.score)
-						bestMove = evaluatedMovesSpan[i];
-				}
-			}
-
-			return bestMove;
-		}
-
-		public EvaluatedMove EvaluateMove(Move move, Board board, Timer timer, int currentDepth, int recursiveDepth = baseMoveDepth) {
-			EvaluatedMove evalMove = new(move);
-			float score = 0;
-
-			//================================================
-			//If the move is a capture move, how valuable would the captured piece be?
-			if (move.IsCapture) {
-				score += EvaluatePieceValue(move.CapturePieceType);
-				evalMove.interest = GetPieceTakeInterest(move.CapturePieceType, currentDepth);
-			} else {
-				if (move.MovePieceType == PieceType.Knight && SquareIsCloseToCenter(move.TargetSquare)) {
-					score += closerToCentreBonus;
-				} else {
-					score -= closerToCentreBonus;
-				}
-			}
+        }
+    }
 
 
-			//================================================
 
-			//================================================
-			//Each piece has a movement cost, to discourage throwing valuable pieces at the enemy
-			score -= pieceMoveCost[(int)move.MovePieceType];
-			//================================================
+    public class MinMaxFish
+    {
+        public int totalInterest = 0;
+        MOVE[] getLegalMoves(Board board, int startingScore, int moveBudget)
+        {
+            var m = board.GetLegalMoves();
+            var rng = new Random();
+            m = m.OrderBy(e => rng.NextDouble()).ToArray();
 
-			//================================================
-			//Checks next move to see if its advantagous
-			board.MakeMove(move);
-			isMyTurn = !isMyTurn;
 
-			float checkingScore = 0;
 
-			if (board.IsInCheck()) {
-				checkingScore += checkValue;
+            List<MOVE> moves = new List<MOVE>();
+            foreach (Move i in m)
+            {
 
-				if (RandomChanceToPass(40))
-					evalMove.interest = Interest.MEDIUM;
-			}
 
-			if (board.IsInCheckmate()) {
-				if (currentDepth == 1)
-					checkingScore += checkMateValue;
-				else {
-					checkingScore += potentialCheckmateValue;
-				}
+                int localInterest = 1;
+                int localScore = startingScore;
 
-				if (RandomChanceToPass(80))
-					evalMove.interest = Interest.MEDIUM;
-			}
+                //if (i.MovePieceType != PieceType.Pawn || i.MovePieceType != PieceType.King) // encourage using pieces besides pawns/kings
+                //{
 
-			score += checkingScore;
+                //    localScore += 1;
+                //}
 
-			if (board.IsDraw()) {
-				score += drawValue;
-			}
+                //if (i.TargetSquare.File != 0 && i.TargetSquare.File != 7) // discourage moving pieces to edge tiles
+                //{
+                //    localScore += 1;
+                //}
 
-			isMyTurn = !isMyTurn;
-			board.UndoMove(move);
-			//================================================
+                //if (i.TargetSquare.Rank >= 2 && i.TargetSquare.Rank <= 6) // Encourage developing pieces
+                //{
+                //    localScore += 1;
+                //}
 
-			//================================================
-			//Bonus scores given to special moves
-			if (move.IsPromotion)
-				score += promotionBonus;
+                //if (!board.IsInCheck()) // encourage moving the king while in check
+                //{
+                //    localScore += 1;
+                //    // localInterest++;
+                //}
 
-			if (move.IsEnPassant)
-				score += enPassantBonus;
+                if (i.IsCastles) // encourage castling
+                {
+                    localScore += 50;
+                    //      localInterest++;
+                }
+                if (i.IsPromotion) // encourage promotion!
+                {
+                    //     localScore += Def.pieceValues[(int)i.PromotionPieceType];
+                }
 
-			if (move.IsCastles)
-				score += castleBonus;
-			//===============================================
+                if (i.IsCapture) // encourage capture
+                {
+                    // localScore += Def.pieceValues[(int)i.CapturePieceType];
+                    //   localInterest += 1;
+                }
 
-			//Moves that are made from the other player are negative, and are retracted from a moves scoring
-			evalMove.score += (int)(score * TurnMultipler());
+                if (board.SquareIsAttackedByOpponent(i.TargetSquare))
+                {
+                    // localInterest++;
+                    // localScore -= Def.pieceValues[(int)i.MovePieceType] / 2 ;
+                }
 
-			//Bonus depth is added depending on how interesting that move was + time must be above 5 seconds to prevent timeout
-			int depthToExplore = currentDepth == 1 && timer.MillisecondsRemaining > 5000 ? recursiveDepth + (int)evalMove.interest : recursiveDepth;
 
-			//THE SECRET SAUCE // Moves are checked recursively to decide whether a move would be good or not, the moves alternate players
-			if (currentDepth < recursiveDepth) {
-				board.MakeMove(move);
-				isMyTurn = !isMyTurn;
+                board.MakeMove(i);
 
-				Span<Move> moveSpan = stackalloc Move[500];
-				board.GetLegalMovesNonAlloc(ref moveSpan);
+                if (board.IsInCheckmate())
+                {
+                    localScore = int.MaxValue;
+                    localInterest = 0;
+                }
+                if (board.IsInStalemate() || board.IsInsufficientMaterial() || board.IsFiftyMoveDraw() || board.IsRepeatedPosition())
+                {
+                    localScore = (0);
+                    localInterest = 0;
+                }
 
-				if (moveSpan.Length > 0) {
-					Span<EvaluatedMove> evaluatedMovesSpan = stackalloc EvaluatedMove[moveSpan.Length];
 
-					EvaluatedMove nextBestMove = GetBestMove(EvaluateMoves(moveSpan, evaluatedMovesSpan, board, timer, currentDepth + 1, depthToExplore));
-					evalMove.score += nextBestMove.score;
-				}
 
-				board.UndoMove(move);
-				isMyTurn = !isMyTurn;
-			}
+                board.UndoMove(i);
 
-			return evalMove;
-		}
+                totalInterest += localInterest;
+                moves.Add(new MOVE(i, localScore, localInterest));
+            }
 
-		//When its the enemies turn the scores are flipped.
-		static float TurnMultipler() {
-			if (isMyTurn)
-				return 1;
-			else
-				return -enemyTurnMultiplier;
-		}
+            List<MOVE> budgetMoves = new List<MOVE>();
 
-		//Multplier that prioritises protecting high value pieces over taking pieces.
-		static int EvaluatePieceValue(PieceType pieceType) {
-			int pieceValue = pieceValues[(int)pieceType];
+            foreach (MOVE i in moves)
+            {
+                i.setBudget(totalInterest, moveBudget);
+                budgetMoves.Add(i);
+            }
+            return budgetMoves.ToArray();
+        }
 
-			if (isMyTurn)
-				return pieceValue;
-			else
-				return (int)(myPieceMultiplier * pieceValue);
-		}
 
-		static Interest GetPieceTakeInterest(PieceType pieceType, int currentDepth) {
-			switch (pieceType) {
-				case PieceType.Rook:
-				case PieceType.Bishop:
-					if (RandomChanceToPass(50))
-						return Interest.MEDIUM;
-					return Interest.NONE;
+        public MOVE Think(Board board, int moveBudget, int score, int depth)
+        {
+            Random rng = new Random();
 
-				case PieceType.Queen:
-					if (RandomChanceToPass(80))
-						return Interest.MEDIUM;
-					return Interest.NONE;
 
-				default:
-					return Interest.NONE;
-			}
-		}
+            PieceList[] pieces = board.GetAllPieceLists();
 
-		//If square is closer to the center
-		private bool SquareIsCloseToCenter(Square square) {
-			if (square.File > 1 && square.File < 6 && square.Rank > 1 && square.Rank < 6) {
-				return true;
-			}
+            int whiteScore = 0;
+            int blackScore = 0;
 
-			return false;
-		}
+            for (int i = 0; i <= 5; i++)
+            {
+                foreach (Piece piece in pieces[i])
+                {
+                    whiteScore += Def.pieceValues[i + 1];
+                    if (board.SquareIsAttackedByOpponent(piece.Square) && board.IsWhiteToMove)
+                    {
+                        //             whiteScore -= Def.pieceValues[i + 1] /2;
+                    }
+                }
 
-		private static bool RandomChanceToPass(int percentageChance) {
-			//Seed rng
-			if (rng == null) {
-				rng = new Random(((int)DateTime.UtcNow.Ticks));
-			}
-			int randomValue = rng.Next(100);
-			return randomValue < percentageChance;
-		}
+                foreach (Piece piece in pieces[i + 6])
+                {
+                    blackScore += Def.pieceValues[i + 1];
+                    if (board.SquareIsAttackedByOpponent(piece.Square) && !board.IsWhiteToMove)
+                    {
+                        //             blackScore -= Def.pieceValues[i + 1] /2;
+                    }
+                }
+            }
 
-		private static int GetDepthDivision(int currentDepth) {
-			if (currentDepth % 2 == 0) {
-				return currentDepth / 2;
-			} else
-				return (currentDepth + 1) / 2;
-		}
-	}
+
+
+            if (board.IsWhiteToMove) score = whiteScore - blackScore;
+            else score = blackScore - whiteScore;
+
+            if (moveBudget <= 0) score -= 1000;
+
+            MOVE[] moves = getLegalMoves(board, score, moveBudget);
+
+            List<MOVE> bestMoves = new List<MOVE>();
+
+            for (int i = 0; i < Def.moveAvg; i++)
+            {
+                bestMoves.Add(moves[0]);
+            }
+
+            foreach (MOVE move in moves)
+            {
+
+                board.MakeMove(move.move);
+                if (move.Budget >= 1 && !board.IsInCheckmate() && depth > 0)
+                {
+
+                    MinMaxFish i = new MinMaxFish();
+                    MOVE a = i.Think(board, move.Budget - 1, (move.getScore()) * -1, depth - 1);
+
+                    move.setScore(a.getScore() * -1);
+
+
+                }
+                board.UndoMove(move.move);
+
+
+                for (int i = 0; i < Def.moveAvg; i++)
+                {
+                    if (move.getScore() > bestMoves[i].getScore())
+                    {
+                        bestMoves.Add(move);
+                        bestMoves.Remove(bestMoves[i]);
+                        i = Def.moveAvg;
+                    }
+                }
+
+            }
+
+            int totalScore = 0;
+            MOVE bestMove = bestMoves[0];
+            for (int i = 0; i < Def.moveAvg; i++)
+            {
+                totalScore += bestMoves[i].getScore();
+                if (bestMoves[i].getScore() > bestMove.getScore()) bestMove = bestMoves[i];
+            }
+
+
+            bestMove.setScore(  (int) (((totalScore / Def.moveAvg) * 0.1f) + (bestMove.getScore() * 0.9f)) );
+
+            return bestMove;
+
+        }
+
+
+    }
+
+
+    public Move Think(Board board, Timer timer)
+    {
+
+
+
+        MinMaxFish fisher = new MinMaxFish();
+
+
+        float x = (timer.MillisecondsRemaining / 60000.0f);
+        x = (-4 * (float)Math.Pow(x, 2)) + (4 * x);
+        x = (x * 0.75f) + 0.25f;
+        int timeBudget = (int)(Def.moveBudget * x);
+        MOVE bestMove;
+        if (board.IsWhiteToMove) bestMove = fisher.Think(board, timeBudget, 0, Def.maxDepth);
+        else bestMove = fisher.Think(board, timeBudget, 0, Def.maxDepth);
+
+        Console.WriteLine($"My best move score: {bestMove.getScore()}");
+
+        return bestMove.move;
+
+    }
+
+}
 }
